72c26c9ae144e2b837c3b030442a465f5dfa143d
azat syrtbaev <syrttbaev7@gmail.com>
2024-01-30 11:35:21 +0100

    added first Task

--- Program.cs
+++ Program.cs
@@ -5,12 +5,12 @@
 // a = 256 => 26
 // a = 891 => 81
 
-int number = 947;
-int firstDigit = number / 100; // 453 / 100 = 4
-int thirdDigit = number % 10; // 453 % 10 = 3
-int result = firstDigit * 10 + thirdDigit; // 4 * 10 + 3 = 43
-// $"Текст {result} текст {a+100} текст {a - 500}"
-Console.WriteLine($"В числе {number} => {result}");
+// int number = 947;
+// int firstDigit = number / 100; // 453 / 100 = 4
+// int thirdDigit = number % 10; // 453 % 10 = 3
+// int result = firstDigit * 10 + thirdDigit; // 4 * 10 + 3 = 43
+// // $"Текст {result} текст {a+100} текст {a - 500}"
+// Console.WriteLine($"В числе {number} => {result}");
 
 
 
